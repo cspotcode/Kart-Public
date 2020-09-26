06fe784cfced9b685b4334dd1bba0a4b61fb0e79
Hannu Hanhi <hhanhipublic@gmail.com>
2020-09-27 00:34:24 +0300

    More readable stats for non-low resolutions

--- src/m_perfstats.c
+++ src/m_perfstats.c
@@ -26,17 +26,17 @@ int ps_tictime = 0;
 
 void M_DrawPerfStats(void)
 {
-	if (cv_perfstats.value)
+	char s[50];
+	int currenttime = I_GetTimeMicros();
+	int frametime = currenttime - ps_prevframetime;
+	int divisor = 1;
+	ps_prevframetime = currenttime;
+
+//	if (ps_rendercalltime > 10000)
+//		divisor = 1000;
+
+	if (vid.width < 640 || vid.height < 400) // low resolution
 	{
-		char s[50];
-		int currenttime = I_GetTimeMicros();
-		int frametime = currenttime - ps_prevframetime;
-		int divisor = 1;
-		ps_prevframetime = currenttime;
-
-		if (ps_rendercalltime > 10000)
-			divisor = 1000;
-
 		snprintf(s, sizeof s - 1, "frmtime %d", frametime / divisor);
 		V_DrawThinString(20, 10, V_MONOSPACE | V_YELLOWMAP, s);
 		snprintf(s, sizeof s - 1, "drwtime %d", ps_rendercalltime / divisor);
@@ -106,6 +106,79 @@ void M_DrawPerfStats(void)
 			V_DrawThinString(20, 80, V_MONOSPACE | V_YELLOWMAP, s);
 			snprintf(s, sizeof s - 1, "tic     %d", ps_tictime / divisor);
 			V_DrawThinString(20, 95, V_MONOSPACE | V_GRAYMAP, s);
+		}
+	}
+	else // high resolution
+	{
+		snprintf(s, sizeof s - 1, "Frame time:     %d", frametime / divisor);
+		V_DrawSmallString(20, 10, V_MONOSPACE | V_ALLOWLOWERCASE | V_YELLOWMAP, s);
+		snprintf(s, sizeof s - 1, "3d rendering:   %d", ps_rendercalltime / divisor);
+		V_DrawSmallString(20, 15, V_MONOSPACE | V_ALLOWLOWERCASE | V_YELLOWMAP, s);
+		snprintf(s, sizeof s - 1, "RenderBSPNode:  %d", ps_bsptime / divisor);
+		V_DrawSmallString(24, 20, V_MONOSPACE | V_ALLOWLOWERCASE | V_YELLOWMAP, s);
+		snprintf(s, sizeof s - 1, "BSP calls:    %d", ps_numbspcalls);
+		V_DrawSmallString(115, 10, V_MONOSPACE | V_ALLOWLOWERCASE | V_BLUEMAP, s);
+		snprintf(s, sizeof s - 1, "Sprites:      %d", ps_numsprites);
+		V_DrawSmallString(115, 15, V_MONOSPACE | V_ALLOWLOWERCASE | V_BLUEMAP, s);
+		snprintf(s, sizeof s - 1, "Drawnodes:    %d", ps_numdrawnodes);
+		V_DrawSmallString(115, 20, V_MONOSPACE | V_ALLOWLOWERCASE | V_BLUEMAP, s);
+		snprintf(s, sizeof s - 1, "Polyobjects:  %d", ps_numpolyobjects);
+		V_DrawSmallString(115, 25, V_MONOSPACE | V_ALLOWLOWERCASE | V_BLUEMAP, s);
+		#ifdef HWRENDER
+		if (rendermode == render_opengl) // OpenGL specific stats
+		{
+			snprintf(s, sizeof s - 1, "Drwnode sort:   %d", ps_hw_nodesorttime / divisor);
+			V_DrawSmallString(24, 25, V_MONOSPACE | V_ALLOWLOWERCASE | V_YELLOWMAP, s);
+			snprintf(s, sizeof s - 1, "Drwnode render: %d", ps_hw_nodedrawtime / divisor);
+			V_DrawSmallString(24, 30, V_MONOSPACE | V_ALLOWLOWERCASE | V_YELLOWMAP, s);
+			snprintf(s, sizeof s - 1, "Sprite sort:    %d", ps_hw_spritesorttime / divisor);
+			V_DrawSmallString(24, 35, V_MONOSPACE | V_ALLOWLOWERCASE | V_YELLOWMAP, s);
+			snprintf(s, sizeof s - 1, "Sprite render:  %d", ps_hw_spritedrawtime / divisor);
+			V_DrawSmallString(24, 40, V_MONOSPACE | V_ALLOWLOWERCASE | V_YELLOWMAP, s);
+			snprintf(s, sizeof s - 1, "UI render:      %d", ps_uitime / divisor);
+			V_DrawSmallString(20, 45, V_MONOSPACE | V_ALLOWLOWERCASE | V_YELLOWMAP, s);
+			snprintf(s, sizeof s - 1, "I_FinishUpdate: %d", ps_swaptime / divisor);
+			V_DrawSmallString(20, 50, V_MONOSPACE | V_ALLOWLOWERCASE | V_YELLOWMAP, s);
+			snprintf(s, sizeof s - 1, "Game logic:     %d", ps_tictime / divisor);
+			V_DrawSmallString(20, 60, V_MONOSPACE | V_ALLOWLOWERCASE | V_GRAYMAP, s);
+			if (cv_grbatching.value)
+			{
+				snprintf(s, sizeof s - 1, "Batch sort:   %d", ps_hw_batchsorttime / divisor);
+				V_DrawSmallString(115, 35, V_MONOSPACE | V_ALLOWLOWERCASE | V_REDMAP, s);
+				snprintf(s, sizeof s - 1, "Batch render: %d", ps_hw_batchdrawtime / divisor);
+				V_DrawSmallString(115, 40, V_MONOSPACE | V_ALLOWLOWERCASE | V_REDMAP, s);
+
+				snprintf(s, sizeof s - 1, "Polygons:   %d", ps_hw_numpolys);
+				V_DrawSmallString(200, 10, V_MONOSPACE | V_ALLOWLOWERCASE | V_PURPLEMAP, s);
+				snprintf(s, sizeof s - 1, "Vertices:   %d", ps_hw_numverts);
+				V_DrawSmallString(200, 15, V_MONOSPACE | V_ALLOWLOWERCASE | V_PURPLEMAP, s);
+				snprintf(s, sizeof s - 1, "Draw calls: %d", ps_hw_numcalls);
+				V_DrawSmallString(200, 25, V_MONOSPACE | V_ALLOWLOWERCASE | V_PURPLEMAP, s);
+				snprintf(s, sizeof s - 1, "Shaders:    %d", ps_hw_numshaders);
+				V_DrawSmallString(200, 30, V_MONOSPACE | V_ALLOWLOWERCASE | V_PURPLEMAP, s);
+				snprintf(s, sizeof s - 1, "Textures:   %d", ps_hw_numtextures);
+				V_DrawSmallString(200, 35, V_MONOSPACE | V_ALLOWLOWERCASE | V_PURPLEMAP, s);
+				snprintf(s, sizeof s - 1, "Polyflags:  %d", ps_hw_numpolyflags);
+				V_DrawSmallString(200, 40, V_MONOSPACE | V_ALLOWLOWERCASE | V_PURPLEMAP, s);
+				snprintf(s, sizeof s - 1, "Colors:     %d", ps_hw_numcolors);
+				V_DrawSmallString(200, 45, V_MONOSPACE | V_ALLOWLOWERCASE | V_PURPLEMAP, s);
+			}
+		}
+		else // software specific stats
+#endif
+		{
+			snprintf(s, sizeof s - 1, "Portal render:  %d", ps_sw_portaltime / divisor);
+			V_DrawSmallString(24, 25, V_MONOSPACE | V_ALLOWLOWERCASE | V_YELLOWMAP, s);
+			snprintf(s, sizeof s - 1, "R_DrawPlanes:   %d", ps_sw_planetime / divisor);
+			V_DrawSmallString(24, 30, V_MONOSPACE | V_ALLOWLOWERCASE | V_YELLOWMAP, s);
+			snprintf(s, sizeof s - 1, "R_DrawMasked:   %d", ps_sw_maskedtime / divisor);
+			V_DrawSmallString(24, 35, V_MONOSPACE | V_ALLOWLOWERCASE | V_YELLOWMAP, s);
+			snprintf(s, sizeof s - 1, "UI render:      %d", ps_uitime / divisor);
+			V_DrawSmallString(20, 40, V_MONOSPACE | V_ALLOWLOWERCASE | V_YELLOWMAP, s);
+			snprintf(s, sizeof s - 1, "I_FinishUpdate: %d", ps_swaptime / divisor);
+			V_DrawSmallString(20, 45, V_MONOSPACE | V_ALLOWLOWERCASE | V_YELLOWMAP, s);
+			snprintf(s, sizeof s - 1, "Game logic:     %d", ps_tictime / divisor);
+			V_DrawSmallString(20, 55, V_MONOSPACE | V_ALLOWLOWERCASE | V_GRAYMAP, s);
 		}
 	}
 }
