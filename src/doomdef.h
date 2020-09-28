61a7d714633398c43b6ca0bcc4959d33a5c46f62
James R <justsomejames2@gmail.com>
2020-09-28 14:59:35 -0700

    Add bird to the version string

--- src/doomdef.h
+++ src/doomdef.h
@@ -149,8 +149,8 @@ extern char  logfilename[1024];
 #else
 #define VERSION    1 // Game version
 #define SUBVERSION 3 // more precise version number
-#define VERSIONSTRING "v1.3"
-#define VERSIONSTRINGW L"v1.3"
+#define VERSIONSTRING "v1.3 (bird v3)"
+#define VERSIONSTRINGW L"v1.3 (bird v3)"
 // Hey! If you change this, add 1 to the MODVERSION below! Otherwise we can't force updates!
 // And change CMakeLists.txt, for CMake users!
 // AND appveyor.yml, for the build bots!
