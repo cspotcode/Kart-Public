4aadc31ac369c93e0a77cce0bf2bf1bc5f160f1c
Andrew Bradley <cspotcode@gmail.com>
2021-08-30 21:48:41 -0400

    add / fix linux dev container

--- setup-dev-environment.ps1
+++ setup-dev-environment.ps1
@@ -1,24 +1,41 @@
 param(
     [switch]$forceRecreateBuildScripts,
     [switch]$build,
-    [switch]$run
+    [switch]$run,
+    [switch]$runDedicated
 )
 
 $ErrorActionPreference = 'Stop'
 
+# Catering to windows powershell.  Prefer using powershell core, but if you don't have that, we'll do our best
+if(-not $IsLinux -and -not $IsMacOS -and -not $IsWindows) {
+    $IsWindows = $true
+}
+
+$buildDir = "$PSScriptRoot/build"
+$cmakeToolchain = "MinGW Makefiles"
+$binName = "srb2kart.exe"
+if($IsLinux) {
+    $buildDir = "$PSScriptRoot/build-linux"
+    $cmakeToolchain = "Unix Makefiles"
+    $binName = "srb2kart"
+}
+
 $toolchainBaseDir = "$PSScriptRoot/toolchain"
 $toolchainDownloadsDir = "$toolchainBaseDir/downloads"
-$cmakeZipDownload = "$toolchainDownloadsDir/cmake.zip"
-$cmakeZipUri = 'https://github.com/Kitware/CMake/releases/download/v3.21.2/cmake-3.21.2-windows-x86_64.zip'
-$cmakeExtractDir = "$toolchainBaseDir/cmake"
-$cmakeBinDir = "$cmakeExtractDir/cmake-3.21.2-windows-x86_64/bin"
-$cmakeCommand = "$cmakeBinDir/cmake.exe"
 
-if(-not (test-path $cmakeCommand)) {
-    if (-not (test-path $cmakeZipDownload)) {
-        Invoke-WebRequest -uri $cmakeZipUri -OutFile $cmakeZipDownload
+if($IsWindows) {
+    $cmakeZipDownload = "$toolchainDownloadsDir/cmake.zip"
+    $cmakeExtractDir = "$toolchainBaseDir/cmake"
+    $cmakeZipUri = 'https://github.com/Kitware/CMake/releases/download/v3.21.2/cmake-3.21.2-windows-x86_64.zip'
+    $cmakeBinDir = "$cmakeExtractDir/cmake-3.21.2-windows-x86_64/bin"
+    $cmakeCommand = "$cmakeBinDir/cmake.exe"
+    if(-not (test-path $cmakeCommand)) {
+        if (-not (test-path $cmakeZipDownload)) {
+            Invoke-WebRequest -uri $cmakeZipUri -OutFile $cmakeZipDownload
+        }
+        Expand-Archive $cmakeZipDownload $cmakeExtractDir
     }
-    Expand-Archive $cmakeZipDownload $cmakeExtractDir
 }
 
 $installerPath = "$PSScriptRoot/assets/srb2kart-v13-Installer.exe"
@@ -34,44 +51,62 @@ if (-not (Test-Path $installerExtractedPath)) {
     echo "Extracted installer $installerPath into $installerExtractedPath"
 }
 
-$mingwInstallDir = "$toolchainBaseDir/mingw"
-$mingwBinDir = "$mingwInstallDir/bin"
-$mingwGetZip= "$toolchainDownloadsDir/mingw-get.zip"
-$mingwGetZipUri = 'https://sourceforge.net/projects/mingw/files/Installer/mingw-get/mingw-get-0.6.2-beta-20131004-1/mingw-get-0.6.2-mingw32-beta-20131004-1-bin.zip/download'
-$mingwGetExtractedDir = "$mingwInstallDir"
-$mingwGetCommand = "$mingwGetExtractedDir/bin/mingw-get.exe"
-$mingwMakeCommand = "$mingwInstallDir/bin/mingw32-make.exe"
-if (-not (test-path $mingwMakeCommand)) {
-    if (-not (test-path $mingwGetCommand)) {
-        if (-not (test-path $mingwGetZip)) {
-            if(-not (test-path $toolchainDownloadsDir)) { new-item -ItemType Directory $toolchainDownloadsDir }
-            # Invoke-Webrequest does not work, possibly due to user-agent, possibly due to sourceforge server behavior with redirects
-            $webclient = New-Object System.Net.WebClient
-            $webclient.DownloadFile($mingwGetZipUri, $mingwGetZip)
+if($IsWindows) {
+    $mingwInstallDir = "$toolchainBaseDir/mingw"
+    $mingwBinDir = "$mingwInstallDir/bin"
+    $mingwGetZip= "$toolchainDownloadsDir/mingw-get.zip"
+    $mingwGetZipUri = 'https://sourceforge.net/projects/mingw/files/Installer/mingw-get/mingw-get-0.6.2-beta-20131004-1/mingw-get-0.6.2-mingw32-beta-20131004-1-bin.zip/download'
+    $mingwGetExtractedDir = "$mingwInstallDir"
+    $mingwGetCommand = "$mingwGetExtractedDir/bin/mingw-get.exe"
+    $mingwMakeCommand = "$mingwInstallDir/bin/mingw32-make.exe"
+
+    if (-not (test-path $mingwMakeCommand)) {
+        if (-not (test-path $mingwGetCommand)) {
+            if (-not (test-path $mingwGetZip)) {
+                if(-not (test-path $toolchainDownloadsDir)) { new-item -ItemType Directory $toolchainDownloadsDir }
+                # Invoke-Webrequest does not work, possibly due to user-agent, possibly due to sourceforge server behavior with redirects
+                $webclient = New-Object System.Net.WebClient
+                $webclient.DownloadFile($mingwGetZipUri, $mingwGetZip)
+            }
+            Expand-Archive -Path $mingwGetZip -DestinationPath $mingwGetExtractedDir
         }
-        Expand-Archive -Path $mingwGetZip -DestinationPath $mingwGetExtractedDir
+        & $mingwGetCommand install gcc mingw32-make
     }
-    & $mingwGetCommand install gcc mingw32-make
 }
 
-# TODO avoid adding these every time; after the first invocation in a shell, they're already there
-$env:Path = "$mingwBinDir;$cmakeBinDir;$($env:Path)"
+if($IsWindows) {
+    # TODO avoid adding these every time; after the first invocation in a shell, they're already there
+    $env:Path = "$mingwBinDir;$cmakeBinDir;$($env:Path)"
+}
 
-$buildDir = "$PSScriptRoot/build"
 if($forceRecreateBuildScripts) {
     remove-item -Recurse $buildDir
     new-item -ItemType Directory $buildDir/bin
+}
+
+if(-not (test-path $buildDir/bin/chars.kart)) {
     Expand-Archive -Path $installerPath -DestinationPath $buildDir/bin
     remove-item $buildDir/bin/srb2kart.exe
 }
 
 if(-not (test-path $buildDir/CMakeCache.txt)) {
-    cmake -B build -G "MinGW Makefiles"
+    cmake -B $buildDir -G $cmakeToolchain
 }
 
 if($build) {
-    cmake --build build --config Debug -j 10 --
+    cmake --build $buildDir --config Debug -j 10 --
 }
-if($run) {
-    & "$buildDir/bin/srb2kart.exe"
+if($run -or $runDedicated) {
+    $extraArgs = @()
+    if($runDedicated) {
+        $extraArgs = @('-dedicated')
+    }
+    $beforePwd = $PWD.Path
+    try {
+        cd $buildDir/bin
+        echo $PWD.Path
+        & ./$binName @extraArgs
+    } finally {
+        cd $beforePwd
+    }
 }
